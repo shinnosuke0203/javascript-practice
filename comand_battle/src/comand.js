94ee16d78d1e2dd98e6fbced34e3415f81dbdbf1
shinnosuke0203 <91869162+shinnosuke0203@users.noreply.github.com>
2022-03-23 23:12:07 +0900

    Add files via upload

--- comand_battle/src/comand.js
+++ comand_battle/src/comand.js
@@ -1,13 +1,3 @@
-class Message {
-    static printMessage(text) {
-        messageView.innerHTML = text;
-    }
-
-    static addMessage(text) {
-        messageView.innerHTML += text;
-    }
-}
-
 class Friend {
     constructor (name, maxHp, offense, speed, herb, herbPower) {
         this.name = name;
@@ -51,5 +41,39 @@ class Friend {
         }else {
             Message.printMessage(this.name + "の攻撃．．．<br>" + this.target.name + "は倒れている");
         }
+    }
+
+    recovery() {
+        if(this.herb <= 0) {
+            Message.printMessage(this.name + "は薬草を・・・<br>薬草がない！<br>");
+            return;
+        }
+
+        if(this.maxHp == this.hp) {
+            Message.printMessage(this.name + "は薬草を．．．<br>これ以上回復できない！<br>");
+            return;
+        }
+
+        let heal = this.herbPower;
+
+        if(this.maxHp - this.hp < this.herbPower) {
+            heal = this.maxHp - this.hp;
+        }
+
+        this.hp += heal;
+
+        --this.herb;
+
+        Message.printMessage(this.name + "は薬草を飲んだ<br>体力が"　+ heal + "回復した”<br>");
+    }
+}
+
+class Message {
+    static printMessage(text) {
+        messageView.innerHTML = text;
+    }
+
+    static addMessage(text) {
+        messageView.innerHTML += text;
     }
 }
---
+++ comand_battle/src/game.js
@@ -0,0 +1,3 @@
+let friend1 = new Friend("りんたろう", 200, 80, 20, 3, 50);
+friend1.command = "recoveryCommand";
+friend1.action();
